9b66e66399fe57dbaf611cbeb04061cea814af67
mayubhopi <mayuresh00700@gmail.com>
2020-04-06 21:05:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+ // This is my  first javascript code!
+ //console.log('Hello World!');
+//let name = "Mayuresh";
+//console.log(name);
+
+
+//constant variable
+//const interestRate = 0.3;   //const keyword
+
+
+
+//primitive variable types
+//let name = "Mayuresh";
+//let age = 20;
+//let firstName = undefined; //value and type is undefined.
+//let isApproved = true;
+//let selectColor = null; //clear the value of variable.  it type is 'object'
+
+
+
+//reference variable types
+//let person = {               //this is an object
+//    name: "Mayuresh",
+//    age: 20
+//};
+//changing name
+//person.name = "Mayu"
+//or
+//person['name'] = "Bunty"; //or person[selection] = "Bunty"
+//console.log(person);
+//or
+//console.log(person.name);
+
+
+
+
+//arrays. its type is 'object'
+
+//let selectedColors = ['red', 'blue'];
+//selectedColors[2] = 'green';
+//selectedColors[3] = 1;
+//console.log(selectedColors);
+//console.log(selectedColors[0]);
+//console.log(selectedColors.length);
+
+
+
+
+/*functions
+function greet(name, lastName){
+    console.log("Hello " + name + lastName);
+}
+greet("Mayuresh"," Bhopi");
+greet(1);
+
+function square(number){
+    return number*number;
+}
+console.log(square(2));  */
+
+//console.log("First Line\b \\")
+
+function test(arr,num){
+    arr.push(num);
+    return arr;
+    
+}
+arr = [1,2,3,4];
+console.log(test(arr,5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
